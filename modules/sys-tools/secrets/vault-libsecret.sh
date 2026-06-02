df67b72300478719fd10b9d566d0a7f10151cbc5
Peter Cummuskey <peter.cummuskey@westpac.co.nz>
2026-06-02 13:06:05 +1200

    Fixing vault libsecret integration script.

--- modules/sys-tools/secrets/vault-libsecret.sh
+++ modules/sys-tools/secrets/vault-libsecret.sh
@@ -1,35 +1,47 @@
 #!/usr/bin/env bash
+set -e
 
-if [ -n "${DEBUG}" ]; then
-	echo >&2 "${0} ${*}"
-	env >&2
+[ -n "${DEBUG}" ] && echo >&2 "${0} ${*}"
+
+VAULT_HOST="${VAULT_ADDR}"
+if [ -n "${VAULT_HOST}" ]; then
+	# shellcheck disable=SC2001
+	VAULT_HOST="$(sed -r 's|^([^/]+://)?([^/]+).*$|\2|' <<<"${VAULT_HOST}")"
 fi
 
-VAULT_ADDR="${VAULT_ADDR:-unknown}"
-VAULT_NAMESPACE="${VAULT_NAMESPACE:-unknown}"
-VAULT_USER="${VAULT_USER:-unknown}"
+args=(
+	service   vault
+	server    "${VAULT_HOST}"
+	namespace "${VAULT_NAMESPACE:-unknown}"
+	username  "${VAULT_USER:-unknown}"
+)
 
-# shellcheck disable=SC2001
-VAULT_HOST="$(sed -r 's|^(?:.+://)?([^/]+).*$|\1|' <<<"${VAULT_ADDR}")"
+[ -n "${DEBUG}" ] && echo >&2 "${args[*]}"
 
 case "${1}" in
 
 	get)
-		secret-tool lookup \
-			server    "${VAULT_HOST}" \
-			namespace "${VAULT_NAMESPACE}"
+		if token="$(secret-tool lookup "${args[@]}")"; then
+			printf "%s" "${token}"
+		else
+			echo >&2 'No token found.'
+		fi
 		;;
 
 	store)
-		secret-tool store --label="Vault: ${VAULT_NAMESPACE} on ${VAULT_HOST}" \
-			server    "${VAULT_HOST}" \
-			namespace "${VAULT_NAMESPACE}"
+		label="Vault"
+		[ -n "${VAULT_USER}" ] && label+="${VAULT_USER}@"
+		[ -n "${VAULT_HOST}" ] && label+="${VAULT_HOST}"
+		[ -n "${VAULT_NAMESPACE}" ] && label+="#${VAULT_NAMESPACE}"
+		secret-tool store \
+			--label="${label}" \
+			"${args[@]}"
 		;;
 
 	erase)
-		secret-tool clear \
-			server    "${VAULT_HOST}" \
-			namespace "${VAULT_NAMESPACE}"
+		if ! secret-tool clear "${args[@]}"; then
+			echo >&2 'No token found to erase.'
+		fi
 		;;
 
 	*)
