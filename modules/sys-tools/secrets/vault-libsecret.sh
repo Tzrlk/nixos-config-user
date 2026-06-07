f2cb5a2325b016226d5c63c11d9a1de0fafac360
Peter Cummuskey <peter.cummuskey@westpac.co.nz>
2026-06-08 09:59:26 +1200

    Making vault-libsecret script stricter.

--- modules/sys-tools/secrets/vault-libsecret.sh
+++ modules/sys-tools/secrets/vault-libsecret.sh
@@ -3,18 +3,38 @@ set -e
 
 [ -n "${DEBUG}" ] && echo >&2 "${0} ${*}"
 
-VAULT_HOST="${VAULT_ADDR}"
-if [ -n "${VAULT_HOST}" ]; then
-	# shellcheck disable=SC2001
-	VAULT_HOST="$(sed -r 's|^([^/]+://)?([^/]+).*$|\2|' <<<"${VAULT_HOST}")"
+# TODO: If VAULT_ADDR is not set, query secrets to find all the stored hosts
+#       and present an interactive option to select one of them or add another.
+#       If VAULT_NAMESPACE isn't set, do the same thing for selecting a
+#       namespace.
+#       Also prompt for VAULT_USER if it's missing.
+#       Finally, check the token expiry and trigger a login if it's expired,
+#       storing the new expiry on the token as well.
+
+if [ -e "${VAULT_ADDR}" ]; then
+	echo >&2 "Unable to locate token without VAULT_ADDR being set."
+	exit 2
 fi
+
+if [ -e "${VAULT_NAMESPACE}" ]; then
+	echo >&2 "Unable to locate token without VAULT_NAMESPACE being set."
+	exit 2
+fi
+
+# shellcheck disable=SC2001
+VAULT_HOST="$(sed -r 's|^([^/]+://)?([^/]+).*$|\2|' <<<"${VAULT_ADDR}")"
 
 args=(
 	service   vault
 	server    "${VAULT_HOST}"
-	namespace "${VAULT_NAMESPACE:-unknown}"
-	username  "${VAULT_USER:-unknown}"
+	namespace "${VAULT_NAMESPACE}"
 )
+
+if [ -n "${VAULT_USER}" ]; then
+	args+=( username "${VAULT_USER}" )
+else
+	echo >&2 'WARN: Setting VAULT_USER provides more precise token location.'
+fi
 
 [ -n "${DEBUG}" ] && echo >&2 "${args[*]}"
 
@@ -29,10 +49,7 @@ case "${1}" in
 		;;
 
 	store)
-		label="Vault"
-		[ -n "${VAULT_USER}" ] && label+="${VAULT_USER}@"
-		[ -n "${VAULT_HOST}" ] && label+="${VAULT_HOST}"
-		[ -n "${VAULT_NAMESPACE}" ] && label+="#${VAULT_NAMESPACE}"
+		label="Vault: ${VAULT_ADDR}?namespace=${VAULT_NAMESPACE}"
 		secret-tool store \
 			--label="${label}" \
 			"${args[@]}"
