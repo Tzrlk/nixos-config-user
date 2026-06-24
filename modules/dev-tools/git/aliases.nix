4b5c84e87d4d6a1956ae9b6f3ebb063280e4439c
Peter Cummuskey <peter.cummuskey@westpac.co.nz>
2026-06-24 13:33:15 +1200

    Making git aliases spicier.

--- modules/dev-tools/git/aliases.nix
+++ modules/dev-tools/git/aliases.nix
@@ -1,9 +1,18 @@
-{ ... }: {
+{ lib, ... }: with lib; let
+	repeatArgs = flag: items: concatStringsSep " " (map (item: ''${flag} "${item}"'') items);
+	exec       = body: "\$(${body})";
+	fn         = body: "!f() { ${body}; }; f";
+	hardReset  = ref:  ''git reset --hard "${ref}"'';
+	updateRef  = ref: shaTo: shaFrom: ''git update-ref "${ref}" "${shaTo}" "${shaFrom}"'';
+	commitTree = tree: parents: ''git commit-tree "${tree}" ${repeatArgs "-p" parents}'';
+	revParse   = ref: ''git rev-parse "${ref}"'';
+	treeParse  = ref: revParse "${ref}^{tree}";
+in {
 
 	# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.git.aliases
 	programs.git.settings.alias = {
 
-		# General quick shortcuts.
+		## General quick shortcuts ############################################
 		st = "status -a";
 		cl = "clone";
 		ci = "commit";
@@ -13,18 +22,39 @@
 		dc = "diff --cached";
 		rs = "reset";
 
-		# Graph display.
+		## Log display ########################################################
 		logr = "log --graph --decorate --oneline --abbrev-commit";
 		logra = "logr --all";
 
-		# Repo maintenance.
-		fap = "fetch --all --prune";
-		reset-from = ''!f() { git update-ref "refs/heads/''${1}" "''$(git rev-parse HEAD)" "''$(git rev-parse "''${1}")"; }; f'';
+		## Repo maintenance ###################################################
 
-		# Laziness
-		amend = "commit --amend --no-edit";
-		remain = ''!git reset-from main && git checkout main'';
-		jenk  = "!git add Jenkinsfile && git amend && git push -f";
+		## Reset From #################
+		# Hard-resets a branch to the current commit.
+		reset-from = fn (updateRef
+			"refs/heads/\${1}"
+			(exec (revParse "HEAD"))
+			(exec (revParse "\${1}")));
+
+		## Reset Into #################
+		# Same as `reset-from`, but it also checks the branch out.
+		reset-into = fn ''git reset-from "''${1}" && git co "''${1}"'';
+
+		## Merge Theirs ###############
+		# Merges the target branch into the current one, replacing the current
+		# branches' content entirely with that of the target.
+		merge-theirs = fn (hardReset
+			(exec (commitTree (exec (treeParse "\${1}")) [
+				(exec (revParse "HEAD"))
+				(exec (revParse "\${1}"))
+			])));
+
+		## Laziness ###########################################################
+		amend  = "commit --amend --no-edit";
+		remain = "reset-into main";
+		jenk   = "!git add Jenkinsfile && git fixup";
+		fixup  = "!git amend && git push -f";
+		yolo   = "!git add . && git fixup";
+		fap    = "fetch --all --prune";
 
 	};
 
